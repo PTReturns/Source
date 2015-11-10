de38cda604e5d9718c7a8e4a2ffb939096c1d33d
PTReturns <pstalereturns@gmail.com>
2015-11-10 18:39:06 -0200

    ytguygh

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 #include "Main.h"
 
+
 extern void Network_Packet( smPacket* Packet );
+
+extern DWORD __cdecl readConfigHook( );
 
 CMain::CMain( )
 {
@@ -11,4 +14,5 @@ CMain::CMain( )
 void CMain::Hook( )
 {
 	*( UINT* )( PSTALE + 0x0 ) = ( UINT )&Network_Packet;
+	*( DWORD* )0x04BC5740 = ( DWORD )&readConfigHook;
 }
